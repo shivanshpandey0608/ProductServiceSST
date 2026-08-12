54ae614265af89f34b826db6731b886f103f8621
Shivansh Pandey <you@example.com>
2026-08-12 14:23:41 +0530

    Some Changes

--- src/main/java/com/sst/productservicesst/models/Product.java
+++ src/main/java/com/sst/productservicesst/models/Product.java
@@ -1,27 +1,32 @@
 package com.sst.productservicesst.models;
 
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
+@Entity
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String description;
     private Double price;
-    private Category category;
-    private String image;
 
-//    Product(Long id, String title, String description, Double price, Category category, String image)
-//    {
-//        this.id = id;
-//        this.price = price;
-//    }
+    @ManyToOne
+    private Category category;
+
+    private String image;
 }
