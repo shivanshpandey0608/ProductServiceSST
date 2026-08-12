5e09b20e4fc95c9f14a7e7ba658344204003792c
Shivansh Pandey <you@example.com>
2026-08-12 17:29:22 +0530

    Fixed the error part

--- src/main/java/com/sst/productservicesst/exceptionhandlers/GlobalExceptionHandler.java
+++ src/main/java/com/sst/productservicesst/exceptionhandlers/GlobalExceptionHandler.java
@@ -1,27 +1,43 @@
 package com.sst.productservicesst.exceptionhandlers;
 
+import com.sst.productservicesst.dtos.ExceptionDto;
+import com.sst.productservicesst.exceptions.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.http.ResponseEntity;
-import com.sst.productservicesst.dtos.ExceptionDto;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(ArithmeticException.class)
-    public ResponseEntity<ExceptionDto> handleArithmeticException(){
+    public ResponseEntity<ExceptionDto> handleArithmeticException() {
+
         ExceptionDto dto = new ExceptionDto();
         dto.setMessage("Something went wrong");
         dto.setResolution("Arithmetic Exception");
-        ResponseEntity<ExceptionDto> response =new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
-        return response;
+
+        return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler(ArrayIndexOutOfBoundsException.class)
-    public ResponseEntity<ExceptionDto> handleArrayIndexOutOfBoundsException(){
+    public ResponseEntity<ExceptionDto> handleArrayIndexOutOfBoundsException() {
+
         ExceptionDto dto = new ExceptionDto();
         dto.setMessage("Something went wrong");
         dto.setResolution("ArrayIndexOutOfBoundsException");
-        ResponseEntity<ExceptionDto> response =new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
-        return response;
+
+        return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ExceptionDto> handleProductNotFoundException(
+            ProductNotFoundException exception) {
+
+        ExceptionDto dto = new ExceptionDto();
+        dto.setMessage(exception.getMessage());
+        dto.setResolution("Please provide a valid product ID");
+
+        return new ResponseEntity<>(dto, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/sst/productservicesst/exceptions/ProductNotFoundException.java
+++ src/main/java/com/sst/productservicesst/exceptions/ProductNotFoundException.java
@@ -1,8 +1,8 @@
 package com.sst.productservicesst.exceptions;
 
-public class ProductNotFoundException extends RuntimeException{
-    public ProductNotFoundException(String message)
-    {
+public class ProductNotFoundException extends RuntimeException {
+
+    public ProductNotFoundException(String message) {
         super(message);
     }
 }
